0504b7cf60fdfd09ac594c1478ebc2b0c997c757
Scott Gordon <sagordon-dev@outlook.com>
2021-08-04 00:53:28 -0400

    Movrandom number generator function to inside message generator function

--- app.js
+++ app.js
@@ -109,30 +109,39 @@ const adjectiveList = [
   "colorful",
 ];
 
-const randomNumberGenerator = (num) => {
-  let randomNumber = Math.floor(Math.random() * num);
-  return randomNumber;
-};
-
 const randomMessageGenerator = () => {
-  return `When I go to the arcade with my ${pluralNounList[randomNumberGenerator(20)]} 
+  const randomNumberGenerator = (num) => {
+    let randomNumber = Math.floor(Math.random() * num);
+    return randomNumber;
+  };
+
+  let arcadeMadLib = `When I go to the arcade with my ${
+    pluralNounList[randomNumberGenerator(20)]
+  } 
 there are lots of games to play. I spend lots of time there with my friends. 
-In the game X-Men you can be different ${pluralNounList[randomNumberGenerator(20)]}. 
+In the game X-Men you can be different ${
+    pluralNounList[randomNumberGenerator(20)]
+  }. 
 The point of the game is to ${verbList[randomNumberGenerator(20)]} every robot. 
 You also need to save people. Then you can go to the next level.
     
-    In the game Star Wars you are Luke Skywalker and you try to destroy every 
+In the game Star Wars you are Luke Skywalker and you try to destroy every 
 ${nounList[randomNumberGenerator(20)]}. In a car racing/motorcycle racing game 
 you need to beat every computerized vehicle that you are 
 ${ingVerbList[randomNumberGenerator(20)]} against.
 
-    There are a whole lot of other cool games. When you play some games you win 
+There are a whole lot of other cool games. When you play some games you win 
 ${pluralNounList[randomNumberGenerator(20)]} for certain scores. Once you're 
-done you can cash in your tickets to get a big ${nounList[randomNumberGenerator(20)]}. 
-You can save your ${pluralNounList[randomNumberGenerator(20)]} for another time. 
+done you can cash in your tickets to get a big ${
+    nounList[randomNumberGenerator(20)]
+  }. 
+You can save your ${
+    pluralNounList[randomNumberGenerator(20)]
+  } for another time. 
 When I went to this arcade I didn't believe how much fun it would be. So far I 
 have had a lot of fun every time I've been to this great arcade!`;
 
+  return arcadeMadLib;
 };
 
 console.log(randomMessageGenerator());
